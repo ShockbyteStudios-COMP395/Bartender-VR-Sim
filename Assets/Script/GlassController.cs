c3232511adc976effd881653e54a9acc2afb7ee7
Jesus Lawrence Osorio <97567506+JLDO2105@users.noreply.github.com>
2023-04-17 13:21:26 -0400

    Fix some bugs in the spoon and shaker

--- Assets/Script/GlassController.cs
+++ Assets/Script/GlassController.cs
@@ -13,6 +13,7 @@ public class GlassController : MonoBehaviour
     // Start is called before the first frame update
     private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log(collision.gameObject.name);
         if (collision.gameObject.tag.Equals("Shaker"))
         {
             shaked = true;
